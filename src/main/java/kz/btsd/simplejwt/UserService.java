ff9c51b5671beff514ffa7722ff166ec554725f5
Aibek Yergaziyev <aibek.yergaziyev@btsdigital.kz>
2020-04-19 02:42:12 +0600

    Fix password

--- src/main/java/kz/btsd/simplejwt/UserService.java
+++ src/main/java/kz/btsd/simplejwt/UserService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 @Service
 public class UserService {
     public User findByUsername(String username) {
-        return new User("", "", true, true, true, true,
+        return new User("aibek", "$2a$12$lhmPFkhadtA28G.awvmMweMB9mLQsC7CMbgUqjr9dvsBI2eFf5cTu", true, true, true, true,
                 new ArrayList<>());
     }
 }
